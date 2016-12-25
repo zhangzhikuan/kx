fc95d8f14455a62bba957ac74396f97ad309b10b
zhangzhikuan <zhangzhikuan@yongche.com>
2016-12-25 20:57:12 +0800

    dataTabel 远程数据

--- app/controllers/TableController.scala
+++ app/controllers/TableController.scala
@@ -4,21 +4,26 @@ import javax.inject._
 
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import scala.util.parsing.json.{JSONArray, JSONObject}
+
+case class DomainInfo(id: String, domainName: String, price: String, clicks: String, updateTime: String, status: String) {
+  override def toString: String = {
+    val map = Map("id" -> id, "domainName" -> domainName, "price" -> price, "clicks" -> clicks, "updateTime" -> updateTime, "status" -> status)
+    JSONObject(map).toString()
+  }
+}
+
 @Singleton
 class TableController @Inject() extends Controller {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def tables = Action {
     Ok(views.html.app.tables())
   }
 
+  def tableData = Action {
+    val domainList = (1 to 10).map(x => DomainInfo(x.toString, "2", "3", "4", "5", "6")).toList
+    val json = JSONArray(domainList).toString()
+    Ok(json).as("application/json")
+
+  }
 }
